1f48bfaff897fea4974276846208b5052d9635eb
Benedetto-Bonaccorso <BenedettoBonaccorso00@gmail.com>
2022-10-10 17:56:44 +0200

    correzione logica if-else

--- js/main.js
+++ js/main.js
@@ -1,30 +1,25 @@
 let lista = document.querySelector("#lista")
 
-        let message;
-        let attribute;
+let message;
+let attribute;
 
-        for(let i=1; i<=100; i++){
+for(let i=1; i<=100; i++){
 
-            if(i%3==0){
-                message += "Fizz"
-                attribute ="aqua"
-            }
+    if(i%5==0 && i%3==0){
+        message = "FizzBuzz"
+        attribute= "blue"
+    } else if(i%5==0){
+        message = "Buzz"
+        attribute = "green"
+    } else if(i%3==0){
+        message = "Fizz"
+        attribute = "aqua"
+    }else{
+        message = i
+        attribute ="yellow"
+    }
 
-            if(i%5==0){
-                message += "Buzz"
-                attribute ="green"
-            }
-
-            if(i%5==0 && i%3==0){
-                attribute="blue"
-            }
-
-            if(i%3!=0 && i%5!=0){
-                message = i
-                attribute ="yellow"
-            }
-
-            lista.innerHTML += `<p class="col-1 m-1 ${attribute}">${message}</p>`
-            
-            message = ""
-        }
+    lista.innerHTML += `<p class="col-1 m-1 ${attribute}">${message}</p>`
+    
+    message = ""
+}
