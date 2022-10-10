29fb3403dbe03a52af3b46a625f806b012694fc9
Benedetto-Bonaccorso <BenedettoBonaccorso00@gmail.com>
2022-10-10 14:10:14 +0200

    bonus-2

--- js/main.js
+++ js/main.js
@@ -1,22 +1,30 @@
 let lista = document.querySelector("#lista")
 
         let message;
+        let attribute;
 
         for(let i=1; i<=100; i++){
 
             if(i%3==0){
                 message += "Fitz"
+                attribute ="aqua"
             }
 
             if(i%5==0){
                 message += "Buzz"
+                attribute ="green"
+            }
+
+            if(i%5==0&&i%3==0){
+                attribute="blue"
             }
 
             if(i%3!=0 && i%5!=0){
                 message = i
+                attribute ="yellow"
             }
 
-            lista.innerHTML += `<li>${message}</li>`
+            lista.innerHTML += `<p class="col-1 m-1 ratio-1x1 ${attribute}">${message}</p>`
             
             message = ""
         }
